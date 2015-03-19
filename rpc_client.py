06773dd99cef5151230313da398803b783e31f38
Hadrian <had.indi60@gmail.com>
2015-03-19 11:17:01 +0700

    Update rpc_client.py

--- rpc_client.py
+++ rpc_client.py
@@ -1,85 +1,85 @@
 import rpyc
+import os
 import threading
 import time
-import os
 import shutil
 
-class ClientConn:
+
+class Client:
     def __init__(self, host, port):
+        self.conn = rpyc.connect(host, int(port))
         self.host = host
         self.port = port
-        self.conn = rpyc.connect(host, int(port))
 
 class Main:
-    def __init__(self, serverAddress, datasetDir, convertedDir, imgTotal):
+
+    def __init__(self, serverAddress, datasetDir, convertedDir, distrib):
         self.serverAddress = serverAddress
         self.datasetDir = datasetDir
         self.convertedDir = convertedDir
-        self.imgTotal = int(imgTotal)
-        self.clrImg = os.listdir(self.datasetDir)
-        self.node_list = []
+        self.distrib = int(distrib)
+        self.datasetFile = os.listdir(self.datasetDir)
+        self.serverList = []
         self.threads = []
         self.elapsed_time = 0
 
-    def worker(self, node, clrImg):
-        for x in clrImg:
-            print node.host, x
-            loc = open(self.datasetDir + x, 'rb')
-            remote = node.conn.root.openImg(x, 'wb')
-            shutil.copyfileobj(loc, remote)
-            loc.close()
+    def worker(self, node, datasetFile):
+        for filename in datasetFile:
+            #print node.host + " >> " + filename +"\n"
+            local = open(self.datasetDir + filename, 'rb')
+            remote = node.conn.root.openFile(filename, 'wb')
+            shutil.copyfileobj(local, remote)
+            local.close()
             remote.close()
-            node.conn.root.convertImage(x)
-            loc = open(self.convertedDir + x, 'wb')
-            remote = node.conn.root.open('converted_' + x, 'rb')
-            shutil.copyfileobj(remote, loc)
-            loc.close()
+            node.conn.root.convertImage(filename)
+            local = open(self.convertedDir + filename, 'wb')
+            remote = node.conn.root.openFile('converted_' + filename, 'rb')
+            shutil.copyfileobj(remote, local)
+            local.close()
             remote.close()
-            node.conn.delImg(x)
+            node.conn.root.delete(filename)
 
     def run(self):
-        for x in self.serverAddress:
-            host, port = x.split(':')
-            print host, port
-            self.node_list.append(ClientConn(host, port))
+        for address in self.serverAddress:
+            host, port = address.split(':')
+            self.serverList.append(Client(host, port))
 
-        serverCounter = 0
-
-        t_start = time.time()
         i = 0
-        j = self.imgTotal
-        while True:
-            if len(self.clrImg) == 0:
-                break
-            if len(self.clrImg) == i:
-                break
-            if len(self.node_list) == serverCounter:
-                serverCounter = 0 #0 lagi kalo udah ke server terakhir
+        j = self.distrib
+        server_selector = 0
 
-            t = threading.Thread(target = self.worker, args = (self.node_list[serverCounter], self.clrImg[i:j]))
+        start_time = time.time()
+        while True:
+            if len(self.datasetFile) == 0:
+                break
+
+            if i == len(self.datasetFile):
+                break
+
+            if server_selector == len(self.serverList):
+                server_selector = 0
+
+            t = threading.Thread(target=self.worker, args=(self.serverList[server_selector], self.datasetFile[i:j]))
             self.threads.append(t)
             t.start()
+            for files in range(i, j):
+                del self.datasetFile[0]
 
-            for x in range(i, j):
-                del self.clrImg[0]
-
-            serverCounter += 1
+            server_selector += 1
 
         for thread in self.threads:
             thread.join()
 
-        self.elapsed_time = time.time() - t_start
+        self.elapsed_time = time.time() - start_time
 
 if __name__ == "__main__":
 
-    serverAddress = ["127.0.0.1:1337"]
-    print serverAddress
-    datasetDir = "D:\\datasetimg\\"
-    convertedDir = "D:\\converted\\"
-    print "jumlah img yang mau di convert ?"
-    imgTotal = raw_input()
+    serverAddress = ["127.0.0.1:8888"]
+    datasetDir = "D:\datasetimg\\"
+    convertedDir = "D:\converted\\"
+    distrib = 5
 
-    main = Main(serverAddress, datasetDir, convertedDir, imgTotal)
+    main = Main(serverAddress, datasetDir, convertedDir, distrib)
     main.run()
 
     print main.elapsed_time
