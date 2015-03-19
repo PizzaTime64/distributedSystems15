9b4d2bb202809965ffa5593403bdd1505403aa36
Hadrian <had.indi60@gmail.com>
2015-03-19 11:17:37 +0700

    Update rpc_server.py

--- rpc_server.py
+++ rpc_server.py
@@ -1,38 +1,33 @@
-import cv2
 import rpyc
-import sys
+import cv2
 import os
 from rpyc.utils.server import ThreadedServer
 
-class Greyscaler(rpyc.Service):
 
+class Greyscaler(rpyc.Service):
     def on_connect(self):
-        #code that runs when a connection is created
-        # (to init the service, if needed)
-        pass
+        print "someone's connected!"
 
     def on_disconnect(self):
-        #code that runs when the connection has already closed
-        # (to finalize the service, if needed)
         pass
 
-    #exposed : bisa di akses dari luar
-    def exposed_convertImage(self, clrImg):
-        img = cv2.imread(clrImg)
+    def exposed_openFile(self, filename, mode):
+        return open(filename, mode)
+        print filename
+
+    def exposed_convertImage(self, rgb_image):
+        image = cv2.imread(rgb_image)
         convert = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('converted_' + clrImg, convert)
+        cv2.imwrite('converted_' + rgb_image, convert)
 
-    def exposed_openImg(self, namaFile, mode):
-        return open(namaFile, mode)
-
-    def exposed_delImg(self, namaFile):
-        os.remove(namaFile)
-        os.remove('converted_' + namaFile)
+    def exposed_delete(self, filename):
+        os.remove(filename)
+        os.remove('converted_' + filename)
 
 if __name__ == "__main__":
-    hostname = "127.0.0.1"
-    port = 1337
+   
+    hostname = raw_input()
+    port = raw_input()
     print hostname, port
-
-    t = ThreadedServer(Greyscaler, hostname=hostname, port = int(port), protocol_config={"allwo_public_attrs": True})
+    t = ThreadedServer(Greyscaler, hostname=hostname, port=int(port), protocol_config={"allow_public_attrs": True})
     t.start()
